120126bddf0aa1fa2cb06e9f1700c227d014e17e
jhesilva <jheferson.ms@gmail.com>
2021-08-19 23:18:48 +0000

    Atualização

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 import sqlite3
 from flask import Flask, request, session, g, redirect, abort, render_template, flash
 
+# configuração
+DATABASE = "blog.db"
+SECRET_KEY = "pudim"
+
+def conectar_bd():
+    return sqlite3.connect(app.config["DATABASE"])
+
 app = Flask(__name__)
+app.config.from_object(__name__)
 
 @app.route('/hello')
 def pagina_inicial():
