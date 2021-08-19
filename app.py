ce7934e4091d2bf9242aa54a46be972d60d97f10
jhesilva <jheferson.ms@gmail.com>
2021-08-19 23:59:19 +0000

    Atualização

--- app.py
+++ app.py
@@ -17,7 +17,12 @@ def antes_requisicao():
 
 @app.teardown_request
 def depois_request(exc):
-    g.bd.close()    
+    g.bd.close()   
+
+@app.route('/')
+@app.route('/entradas')
+def exibir_entradas():
+    return render_template('exibir_entradas.html')
 
 @app.route('/hello')
 def pagina_inicial():
